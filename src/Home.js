6bf447fff06acbc32da7c8edbdacbcba934b5dc7
azzy kesuma <official.azzy@gmail.com>
2022-04-02 08:24:30 +0700

    adding router

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,13 @@
-import { useState, useEffect } from 'react';
+
 import Bloglist from './Bloglist';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs,setBlogs] = useState(null)
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-            })
-    },[]);
+    const { data:blogs, isPending, error } = useFetch('http://localhost:8000/blogs')  
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            { isPending && <div>Loading...</div> }
            { blogs && <Bloglist blogs = {blogs}/>}
         </div>
     );
